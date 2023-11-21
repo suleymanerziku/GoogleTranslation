6cb3e4427c9da81849b5a97480b9aa55a3962b0d
suleymanerziku <suleymanerziku@gmail.com>
2023-11-21 09:51:26 +0300

    Update on the GUI and README.md Added

---
+++ README.md
@@ -0,0 +1,11 @@
+# Google Translation App
+This app uses tkinter, customtkinter and googletrans library to creat interactive translation app.
+
+**Used Libraries**
+* tkinter
+* customtkinter
+* googletrans
+
+***
+!['Google Translation'](./asset/googleTranlation.png)
+***
--- googleTranslation.py
+++ googleTranslation.py
@@ -3,27 +3,42 @@ import customtkinter
 from tkinter import ttk
 from googletrans import LANGUAGES, Translator
 
-
+# # LANGUAGE TRANSLATION FUNCTION 
 def Translate():
     translator = Translator()
-    translated = translator.translate(text_.get(), dest= lang_menu.get())
-    translate_text.delete(0.0, 'end')
-    translate_text.insert(0.0, translated.text)
-    translate_text.pack(padx = 10, pady = 10)
+    translated_text = translator.translate(user_text.get(index1= 0.0, index2='end'), dest= lang_menu.get())
+    
+    translated_text_box.delete(0.0, 'end')
+    translated_text_box.insert(0.0, translated_text.text)
 
 # SYSTEM SETTINGS
 customtkinter.set_appearance_mode('System')
 customtkinter.set_default_color_theme('blue')
 
-
 # OUR APP FRAME
 app = customtkinter.CTk()
-app.geometry('780x480')
+app.geometry('880x480')
 app.title('Google Translation')
 
-# ADDING UI ELEMENTS
+# THE APP TITLE
 title = customtkinter.CTkLabel(app, text = 'Google Translation', font = ('Arial', 24))
-title.pack(padx = 10, pady = 10)
+title.grid(row = 0, column = 0, columnspan = 2, padx = 10, pady = 10)
+
+# TEXT INPUT
+user_text = customtkinter.CTkTextbox(
+    app,
+    width = 400,  height = 350,
+    font = ('Arial', 20),
+    )
+user_text.grid(row = 1, column = 0, padx = 10, pady = 10)
+
+# DISPLAY TRANSLATED TEXT
+translated_text_box = customtkinter.CTkTextbox(
+    app, width= 400, height= 350,  corner_radius=5,
+    font = ('Arial', 20)
+
+    )
+translated_text_box.grid(row = 1, column = 1, padx = 10, pady = 10)
 
 # SELECT DESTINATION LANGUAGE
 lang_var = tk.StringVar() 
@@ -34,25 +49,16 @@ lang_menu = customtkinter.CTkOptionMenu(
     corner_radius = 3, 
     values = langauges,
     variable = lang_var,
-    
+    font = ('Arial', 12)
+
     )
-lang_menu.pack(padx = 10, pady = 10)
-
-
-# TEXT INPUT
-text_var = tk.StringVar()
-text_ = customtkinter.CTkEntry(app, width = 350,  height = 60, textvariable = text_var)
-text_.pack(padx = 10, pady = 10)
-
+lang_menu.grid(row = 2, column = 0, padx = 10, pady = 10, ipadx = 2, ipady = 2)
+lang_menu.set('Choose Language')
 # TRANSLATE BUTTON
-translate = customtkinter.CTkButton(app, text = 'Translate', command = Translate)
-translate.pack(padx = 10, pady = 10)
-
-# DISPLAY TRANSLATED TEXT
-translate_text = customtkinter.CTkTextbox(app, width= 350, height= 60,  corner_radius=5)
-# translate_text.pack(padx = 10, pady = 10)
-
-
-
+translate_button = customtkinter.CTkButton(
+    app, text = 'Translate', command = Translate,
+    font= ('Arial', 14)
+    )
+translate_button.grid(row = 2, column = 1, padx = 10, pady = 10)
 
 app.mainloop()
